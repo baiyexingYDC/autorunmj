870b6d61ac5dd968b5cad6fb3e7a0878d190d3e0
dac.yang <dac.yang@sunyard.com>
2023-03-09 18:15:45 +0800

    gogo

--- main.py
+++ main.py
@@ -1,51 +1,19 @@
 import time
+import discardOperate
+import guiOperate
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+# 定义一个变量来控制循环
+running = True
 
-
-
-def setUserName():
-  # 定义用户名
-  userNamePre = "say-so-"
-  num = 1
-  # 邀请链接
-  url = "https://discord.gg/KK7bF6KAVt"
-
-  options = webdriver.EdgeOptions()
-  options.add_argument("-inprivate")
-  driver = webdriver.Edge(options=options)
-  # 反爬虫
-  driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    "source": """
-      // 修改navigator.webdriver检测
-      Object.defineProperty(navigator, 'webdriver', {
-        get: () => undefined
-      })
-      // 修改window.navigator属性值
-      window.navigator.chrome = {
-        runtime: {},
-        // etc.
-      }
-      // 修改插件信息
-      const originalQuery = window.navigator.permissions.query;
-      window.navigator.permissions.query = (parameters) => (
-        parameters.name === 'notifications' ?
-          Promise.resolve({ state: Notification.permission }) :
-          originalQuery(parameters)
-      );
-    """
-  })
-  driver.get(url)
-  inputElement = driver.find_element(By.NAME, "username")
-  num = num + 1
-  userName = userNamePre + str(num)
-  inputElement.send_keys(userName)
-  time.sleep(1)
-  # 定位到button type="submit"的元素
-  button = driver.find_element(By.XPATH, "//button[@type='submit']")
-  # 点击该元素
-  button.click()
-  time.sleep(20)
-
-setUserName()
+# 使用try-except语句来捕获键盘中断异常
+try:
+    # 当running为True时，循环执行函数
+    while running:
+        discardOperate.setUserName(14)
+        # guiOperate.writeTokenToExcel()
+        # 可以在这里添加一些延时或其他操作，例如：
+        time.sleep(100)
+except KeyboardInterrupt:
+    # 当按下ctrl+c时，设置running为False并退出循环
+    running = False
+    print('程序已停止')
